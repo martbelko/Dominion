678b2f42e2f7d376717aee9d6416526d6222505c
Martin <martbelko@gmail.com>
2020-09-12 22:03:38 +0200

    Added precompiled header files

---
+++ Dominion/src/dmpch.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <array>
+#include <vector>
+#include <string>
+#include <deque>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+
+#include <algorithm>
+#include <functional>
+#include <memory>
