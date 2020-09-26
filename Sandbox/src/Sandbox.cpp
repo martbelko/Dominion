7234be3a5adae0c6ade44eb1f195560fc6b9758c
Martin <martbelko@gmail.com>
2020-09-27 00:39:21 +0200

    Removed unused function

--- Sandbox/src/Sandbox.cpp
+++ Sandbox/src/Sandbox.cpp
@@ -1,19 +1,11 @@
 #include <Dominion.h>
 
-#include <memory>
-
-void Func(Dominion::MouseScrolledEvent& e)
-{
-	std::cout << "Works\n";
-}
-
 class Sandbox : public Dominion::Application
 {
 public:
 	Sandbox()
 	{
 		Dominion::Event& x = Dominion::MouseMovedEvent(1254.1f, -5.4f);
-
 	}
 
 	~Sandbox()
