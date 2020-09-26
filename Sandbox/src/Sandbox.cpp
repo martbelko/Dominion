70bcdd9a615092a962339cafcc28f637fdd621da
Martin <martbelko@gmail.com>
2020-09-26 21:53:07 +0200

    Testing events

--- Sandbox/src/Sandbox.cpp
+++ Sandbox/src/Sandbox.cpp
@@ -1,10 +1,18 @@
 #include <Dominion.h>
 
+#include <memory>
+
+void Func(Dominion::MouseScrolledEvent& e)
+{
+	std::cout << "Works\n";
+}
+
 class Sandbox : public Dominion::Application
 {
 public:
 	Sandbox()
 	{
+		Dominion::Event& x = Dominion::MouseMovedEvent(1254.1f, -5.4f);
 
 	}
 
