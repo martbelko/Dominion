10d6adff8c7547c05b96089aba02f4aedb913b39
Martin <martbelko@gmail.com>
2020-09-26 13:55:57 +0200

    Added namespace

--- Dominion/src/Dominion/Core/std/String.cpp
+++ Dominion/src/Dominion/Core/std/String.cpp
@@ -1,266 +1,270 @@
 #include "dmpch.h"
 #include "String.h"
 
-String::String(const char* string)
-	: m_String(string)
-{
-}
+namespace Dominion {
 
-String::String(const String& string)
-	: m_String(string.m_String)
-{
-}
+	String::String(const char* string)
+		: m_String(string)
+	{
+	}
 
-String::String(const std::string& string)
-	: m_String(string)
-{
-}
+	String::String(const String& string)
+		: m_String(string.m_String)
+	{
+	}
 
-String& String::operator=(const String& string)
-{
-	m_String = string.m_String;
-	return *this;
-}
+	String::String(const std::string& string)
+		: m_String(string)
+	{
+	}
 
-String& String::operator=(const char* string)
-{
-	m_String = string;
-	return *this;
-}
+	String& String::operator=(const String& string)
+	{
+		m_String = string.m_String;
+		return *this;
+	}
 
-String String::operator+(const String& rhs)
-{
-	return String(m_String + rhs.m_String);
-}
+	String& String::operator=(const char* string)
+	{
+		m_String = string;
+		return *this;
+	}
 
-String& String::operator+=(const String& rhs)
-{
-	return Append(rhs);
-}
+	String String::operator+(const String& rhs)
+	{
+		return String(m_String + rhs.m_String);
+	}
 
-String& String::operator+=(const char* rhs)
-{
-	return Append(rhs);
-}
+	String& String::operator+=(const String& rhs)
+	{
+		return Append(rhs);
+	}
 
-bool String::operator==(const String& rhs)
-{
-	return m_String == rhs.m_String;
-}
+	String& String::operator+=(const char* rhs)
+	{
+		return Append(rhs);
+	}
 
-bool String::operator!=(const String& rhs)
-{
-	return m_String != rhs.m_String;
-}
+	bool String::operator==(const String& rhs)
+	{
+		return m_String == rhs.m_String;
+	}
 
-bool String::operator<(const String& rhs)
-{
-	return m_String < rhs.m_String;
-}
+	bool String::operator!=(const String& rhs)
+	{
+		return m_String != rhs.m_String;
+	}
 
-bool String::operator<=(const String& rhs)
-{
-	return m_String <= rhs.m_String;
-}
+	bool String::operator<(const String& rhs)
+	{
+		return m_String < rhs.m_String;
+	}
 
-bool String::operator>(const String& rhs)
-{
-	return m_String > rhs.m_String;
-}
+	bool String::operator<=(const String& rhs)
+	{
+		return m_String <= rhs.m_String;
+	}
 
-bool String::operator>=(const String& rhs)
-{
-	return m_String >= rhs.m_String;
-}
+	bool String::operator>(const String& rhs)
+	{
+		return m_String > rhs.m_String;
+	}
 
-char& String::operator[](size_t index)
-{
-	return m_String[index];
-}
+	bool String::operator>=(const String& rhs)
+	{
+		return m_String >= rhs.m_String;
+	}
 
-char& String::At(size_t index)
-{
-	return m_String[index];
-}
+	char& String::operator[](size_t index)
+	{
+		return m_String[index];
+	}
 
-String& String::Append(const char* string)
-{
-	m_String.append(string);
-	return *this;
-}
+	char& String::At(size_t index)
+	{
+		return m_String[index];
+	}
 
-String& String::Append(const String& string)
-{
-	m_String.append(string.m_String);
-	return *this;
-}
+	String& String::Append(const char* string)
+	{
+		m_String.append(string);
+		return *this;
+	}
 
-const char* String::Cstr()
-{
-	return m_String.c_str();
-}
+	String& String::Append(const String& string)
+	{
+		m_String.append(string.m_String);
+		return *this;
+	}
 
-size_t String::Length() const
-{
-	return m_String.length();
-}
+	const char* String::Cstr()
+	{
+		return m_String.c_str();
+	}
 
-size_t String::Size() const
-{
-	return m_String.size();
-}
+	size_t String::Length() const
+	{
+		return m_String.length();
+	}
 
-String::Iterator String::begin()
-{
-	return Iterator(m_String.data());
-}
+	size_t String::Size() const
+	{
+		return m_String.size();
+	}
 
-String::Iterator String::end()
-{
-	return Iterator(m_String.data() + m_String.length());
-}
+	String::Iterator String::begin()
+	{
+		return Iterator(m_String.data());
+	}
 
-std::ostream& operator<<(std::ostream& os, const String& string)
-{
-	os << string.m_String;
-	return os;
-}
+	String::Iterator String::end()
+	{
+		return Iterator(m_String.data() + m_String.length());
+	}
 
-std::istream& operator>>(std::istream& is, String& string)
-{
-	is >> string.m_String;
-	return is;
-}
+	std::ostream& operator<<(std::ostream& os, const String& string)
+	{
+		os << string.m_String;
+		return os;
+	}
 
-WString::WString(const wchar_t* string)
-	: m_String(string)
-{
-}
+	std::istream& operator>>(std::istream& is, String& string)
+	{
+		is >> string.m_String;
+		return is;
+	}
 
-WString::WString(const WString& string)
-	: m_String(string.m_String)
-{
-}
+	WString::WString(const wchar_t* string)
+		: m_String(string)
+	{
+	}
 
-WString::WString(const std::wstring& string)
-	: m_String(string)
-{
-}
+	WString::WString(const WString& string)
+		: m_String(string.m_String)
+	{
+	}
 
-WString& WString::operator=(const WString& string)
-{
-	m_String = string.m_String;
-	return *this;
-}
+	WString::WString(const std::wstring& string)
+		: m_String(string)
+	{
+	}
 
-WString& WString::operator=(const wchar_t* string)
-{
-	m_String = string;
-	return *this;
-}
+	WString& WString::operator=(const WString& string)
+	{
+		m_String = string.m_String;
+		return *this;
+	}
 
-WString WString::operator+(const WString& rhs)
-{
-	return WString(m_String + rhs.m_String);
-}
+	WString& WString::operator=(const wchar_t* string)
+	{
+		m_String = string;
+		return *this;
+	}
 
-WString& WString::operator+=(const wchar_t* rhs)
-{
-	m_String += rhs;
-	return *this;
-}
+	WString WString::operator+(const WString& rhs)
+	{
+		return WString(m_String + rhs.m_String);
+	}
 
-WString& WString::operator+=(const WString& rhs)
-{
-	m_String += rhs.m_String;
-	return *this;
-}
+	WString& WString::operator+=(const wchar_t* rhs)
+	{
+		m_String += rhs;
+		return *this;
+	}
 
-bool WString::operator<(const WString& rhs)
-{
-	return m_String < rhs.m_String;
-}
+	WString& WString::operator+=(const WString& rhs)
+	{
+		m_String += rhs.m_String;
+		return *this;
+	}
 
-bool WString::operator<=(const WString& rhs)
-{
-	return m_String <= rhs.m_String;
-}
+	bool WString::operator<(const WString& rhs)
+	{
+		return m_String < rhs.m_String;
+	}
 
-bool WString::operator>(const WString& rhs)
-{
-	return m_String > rhs.m_String;
-}
+	bool WString::operator<=(const WString& rhs)
+	{
+		return m_String <= rhs.m_String;
+	}
 
-bool WString::operator>=(const WString& rhs)
-{
-	return m_String >= rhs.m_String;
-}
+	bool WString::operator>(const WString& rhs)
+	{
+		return m_String > rhs.m_String;
+	}
 
-wchar_t& WString::operator[](size_t index)
-{
-	return m_String[index];
-}
+	bool WString::operator>=(const WString& rhs)
+	{
+		return m_String >= rhs.m_String;
+	}
 
-wchar_t& WString::At(size_t index)
-{
-	return m_String.at(index);
-}
+	wchar_t& WString::operator[](size_t index)
+	{
+		return m_String[index];
+	}
 
-bool WString::operator!=(const WString& rhs)
-{
-	return m_String != rhs.m_String;
-}
+	wchar_t& WString::At(size_t index)
+	{
+		return m_String.at(index);
+	}
 
-bool WString::operator==(const WString& rhs)
-{
-	return m_String == rhs.m_String;
-}
+	bool WString::operator!=(const WString& rhs)
+	{
+		return m_String != rhs.m_String;
+	}
 
-WString& WString::Append(const wchar_t* string)
-{
-	m_String.append(string);
-	return *this;
-}
+	bool WString::operator==(const WString& rhs)
+	{
+		return m_String == rhs.m_String;
+	}
 
-WString& WString::Append(const WString& string)
-{
-	m_String.append(string.m_String);
-	return *this;
-}
+	WString& WString::Append(const wchar_t* string)
+	{
+		m_String.append(string);
+		return *this;
+	}
 
-const wchar_t* WString::Cstr()
-{
-	return m_String.c_str();
-}
+	WString& WString::Append(const WString& string)
+	{
+		m_String.append(string.m_String);
+		return *this;
+	}
 
-size_t WString::Length() const
-{
-	return m_String.length();
-}
+	const wchar_t* WString::Cstr()
+	{
+		return m_String.c_str();
+	}
 
-size_t WString::Size() const
-{
-	return m_String.size();
-}
+	size_t WString::Length() const
+	{
+		return m_String.length();
+	}
 
-WString::Iterator WString::begin()
-{
-	return _Iterator<wchar_t>(m_String.data());
-}
+	size_t WString::Size() const
+	{
+		return m_String.size();
+	}
 
-WString::Iterator WString::end()
-{
-	return _Iterator<wchar_t>(m_String.data() + m_String.length());
-}
+	WString::Iterator WString::begin()
+	{
+		return _Iterator<wchar_t>(m_String.data());
+	}
 
-std::wostream& operator<<(std::wostream& os, const WString& string)
-{
-	os << string.m_String;
-	return os;
-}
+	WString::Iterator WString::end()
+	{
+		return _Iterator<wchar_t>(m_String.data() + m_String.length());
+	}
 
-std::wistream& operator>>(std::wistream& is, WString& string)
-{
-	is >> string.m_String;
-	return is;
+	std::wostream& operator<<(std::wostream& os, const WString& string)
+	{
+		os << string.m_String;
+		return os;
+	}
+
+	std::wistream& operator>>(std::wistream& is, WString& string)
+	{
+		is >> string.m_String;
+		return is;
+	}
+
 }
--- Dominion/src/Dominion/Core/std/String.h
+++ Dominion/src/Dominion/Core/std/String.h
@@ -3,155 +3,159 @@
 #include <iostream>
 #include <string>
 
-template <typename T>
-class _Iterator
-{
-public:
-	_Iterator(T* ptr)
-		: m_Ptr(ptr)
+namespace Dominion {
+
+	template <typename T>
+	class _Iterator
 	{
-	}
+	public:
+		_Iterator(T* ptr)
+			: m_Ptr(ptr)
+		{
+		}
 
-	_Iterator& operator++()
+		_Iterator& operator++()
+		{
+			++m_Ptr;
+			return *this;
+		}
+
+		_Iterator& operator++(int)
+		{
+			_Iterator<T> iterator = *this;
+			++m_Ptr;
+			return iterator;
+		}
+
+		_Iterator& operator--()
+		{
+			--m_Ptr;
+			return *this;
+		}
+
+		_Iterator& operator--(int)
+		{
+			_Iterator iterator = *this;
+			--m_Ptr;
+			return iterator;
+		}
+
+		T& operator[](size_t index)
+		{
+			return *(m_Ptr + index);
+		}
+
+		T* operator->()
+		{
+			return m_Ptr;
+		}
+
+		T& operator*()
+		{
+			return *m_Ptr;
+		}
+
+		bool operator==(const _Iterator& other) const
+		{
+			return m_Ptr == other.m_Ptr;
+		}
+
+		bool operator!=(const _Iterator& other) const
+		{
+			return m_Ptr != other.m_Ptr;
+		}
+	private:
+		T* m_Ptr;
+	};
+
+	class String
 	{
-		++m_Ptr;
-		return *this;
-	}
+	public:
+		using Iterator = _Iterator<char>;
+	public:
+		String() = default;
+		String(const char* string);
+		String(const String& string);
+		~String() = default;
 
-	_Iterator& operator++(int)
+		String& operator=(const char* string);
+		String& operator=(const String& string);
+
+		String operator+(const String& rhs);
+		String& operator+=(const char* rhs);
+		String& operator+=(const String& rhs);
+
+		bool operator==(const String& rhs);
+		bool operator!=(const String& rhs);
+		bool operator<(const String& rhs);
+		bool operator<=(const String& rhs);
+		bool operator>(const String& rhs);
+		bool operator>=(const String& rhs);
+
+		char& operator[](size_t index);
+		char& At(size_t index);
+
+		String& Append(const char* string);
+		String& Append(const String& string);
+		const char* Cstr();
+		size_t Length() const;
+		size_t Size() const;
+
+		Iterator begin();
+		Iterator end();
+
+		friend std::ostream& operator<<(std::ostream& os, const String& string);
+		friend std::istream& operator>>(std::istream& is, String& string);
+	private:
+		String(const std::string& string);
+	private:
+		std::string m_String;
+	};
+
+	class WString
 	{
-		_Iterator<T> iterator = *this;
-		++m_Ptr;
-		return iterator;
-	}
+	public:
+		using Iterator = _Iterator<wchar_t>;
+	public:
+		WString() = default;
+		WString(const wchar_t* string);
+		WString(const WString& string);
 
-	_Iterator& operator--()
-	{
-		--m_Ptr;
-		return *this;
-	}
+		~WString() = default;
 
-	_Iterator& operator--(int)
-	{
-		_Iterator iterator = *this;
-		--m_Ptr;
-		return iterator;
-	}
+		WString& operator=(const wchar_t* string);
+		WString& operator=(const WString& string);
 
-	T& operator[](size_t index)
-	{
-		return *(m_Ptr + index);
-	}
+		WString operator+(const WString& rhs);
+		WString& operator+=(const wchar_t* rhs);
+		WString& operator+=(const WString& rhs);
 
-	T* operator->()
-	{
-		return m_Ptr;
-	}
+		bool operator==(const WString& rhs);
+		bool operator!=(const WString& rhs);
+		bool operator<(const WString& rhs);
+		bool operator<=(const WString& rhs);
+		bool operator>(const WString& rhs);
+		bool operator>=(const WString& rhs);
 
-	T& operator*()
-	{
-		return *m_Ptr;
-	}
+		wchar_t& operator[](size_t index);
+		wchar_t& At(size_t index);
 
-	bool operator==(const _Iterator& other) const
-	{
-		return m_Ptr == other.m_Ptr;
-	}
+		WString& Append(const wchar_t* string);
+		WString& Append(const WString& string);
 
-	bool operator!=(const _Iterator& other) const
-	{
-		return m_Ptr != other.m_Ptr;
-	}
-private:
-	T* m_Ptr;
-};
+		const wchar_t* Cstr();
+		size_t Length() const;
+		size_t Size() const;
 
-class String
-{
-public:
-	using Iterator = _Iterator<char>;
-public:
-	String() = default;
-	String(const char* string);
-	String(const String& string);
-	~String() = default;
+		Iterator begin();
+		Iterator end();
 
-	String& operator=(const char* string);
-	String& operator=(const String& string);
+		friend std::wostream& operator<<(std::wostream& os, const WString& string);
+		friend std::wistream& operator>>(std::wistream& is, WString& string);
+	private:
+		WString(const std::wstring& string);
+	private:
+		std::wstring m_String;
+	};
 
-	String operator+(const String& rhs);
-	String& operator+=(const char* rhs);
-	String& operator+=(const String& rhs);
-
-	bool operator==(const String& rhs);
-	bool operator!=(const String& rhs);
-	bool operator<(const String& rhs);
-	bool operator<=(const String& rhs);
-	bool operator>(const String& rhs);
-	bool operator>=(const String& rhs);
-
-	char& operator[](size_t index);
-	char& At(size_t index);
-
-	String& Append(const char* string);
-	String& Append(const String& string);
-	const char* Cstr();
-	size_t Length() const;
-	size_t Size() const;
-
-	Iterator begin();
-	Iterator end();
-
-	friend std::ostream& operator<<(std::ostream& os, const String& string);
-	friend std::istream& operator>>(std::istream& is, String& string);
-private:
-	String(const std::string& string);
-private:
-	std::string m_String;
-};
-
-class WString
-{
-public:
-	using Iterator = _Iterator<wchar_t>;
-public:
-	WString() = default;
-	WString(const wchar_t* string);
-	WString(const WString& string);
-
-	~WString() = default;
-
-	WString& operator=(const wchar_t* string);
-	WString& operator=(const WString& string);
-
-	WString operator+(const WString& rhs);
-	WString& operator+=(const wchar_t* rhs);
-	WString& operator+=(const WString& rhs);
-
-	bool operator==(const WString& rhs);
-	bool operator!=(const WString& rhs);
-	bool operator<(const WString& rhs);
-	bool operator<=(const WString& rhs);
-	bool operator>(const WString& rhs);
-	bool operator>=(const WString& rhs);
-
-	wchar_t& operator[](size_t index);
-	wchar_t& At(size_t index);
-
-	WString& Append(const wchar_t* string);
-	WString& Append(const WString& string);
-
-	const wchar_t* Cstr();
-	size_t Length() const;
-	size_t Size() const;
-
-	Iterator begin();
-	Iterator end();
-
-	friend std::wostream& operator<<(std::wostream& os, const WString& string);
-	friend std::wistream& operator>>(std::wistream& is, WString& string);
-private:
-	WString(const std::wstring& string);
-private:
-	std::wstring m_String;
-};
+}
