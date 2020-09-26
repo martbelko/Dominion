9ebf59510738e6c2b5c99c4654fbba4cae050e60
Martin <martbelko@gmail.com>
2020-09-26 14:29:40 +0200

    Added log init and log init messages

--- Dominion/src/Dominion/Core/EntryPoint.h
+++ Dominion/src/Dominion/Core/EntryPoint.h
@@ -5,6 +5,8 @@ extern Dominion::Application* Dominion::CreateApplication();
 int main(int argc, char** argv)
 {
 	Dominion::Log::Init();
+	DM_CORE_INFO("Initialized engine core logger");
+	DM_INFO("Initialized client logger");
 
 	Dominion::Application* app = Dominion::CreateApplication();
 	app->Test();
