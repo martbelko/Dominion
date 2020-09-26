e54abfa1c2aba59ad1eaa26009e095982465301b
Martin <martbelko@gmail.com>
2020-09-26 14:29:05 +0200

    Added macros for logging

--- Dominion/src/Dominion/Core/Log.h
+++ Dominion/src/Dominion/Core/Log.h
@@ -17,3 +17,15 @@ namespace Dominion {
 	};
 
 }
+
+#define DM_CORE_TRACE(...) ::Dominion::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define DM_CORE_INFO(...)  ::Dominion::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define DM_CORE_WARN(...)  ::Dominion::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define DM_CORE_ERROR(...) ::Dominion::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define DM_CORE_FATAL(...) ::Dominion::Log::GetCoreLogger()->critical(__VA_ARGS__)
+
+#define DM_TRACE(...) ::Dominion::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define DM_INFO(...)  ::Dominion::Log::GetClientLogger()->info(__VA_ARGS__)
+#define DM_WARN(...)  ::Dominion::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define DM_ERROR(...) ::Dominion::Log::GetClientLogger()->error(__VA_ARGS__)
+#define DM_FATAL(...) ::Dominion::Log::GetClientLogger()->critical(__VA_ARGS__)
