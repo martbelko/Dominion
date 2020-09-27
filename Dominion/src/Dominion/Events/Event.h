e0b10d43be6d4bbfca67223b8e2974b34ce024c9
Martin <martbelko@gmail.com>
2020-09-28 00:55:43 +0200

    Added virtual ToString method, GetName method, operator<< for events

--- Dominion/src/Dominion/Events/Event.h
+++ Dominion/src/Dominion/Events/Event.h
@@ -12,7 +12,8 @@ namespace Dominion {
 	};
 
 	#define EVENT_TYPE(type)    static EventType GetStaticType()    { return type; } \
-                                virtual EventType GetType() const   { return type; }
+                                virtual EventType GetType() const   { return type; } \
+                                virtual const char* GetName() const override { return #type; }
 
 	class Event
 	{
@@ -24,6 +25,8 @@ namespace Dominion {
 		bool IsHandled() const { return m_Handled; }
 
 		virtual EventType GetType() const = 0;
+		virtual const char* GetName() const = 0;
+		virtual std::string ToString() const { return GetName(); }
 
 		template <typename T, typename F>
 		bool Dispatch(F f)
@@ -39,5 +42,10 @@ namespace Dominion {
 	private:
 		bool m_Handled = false;
 	};
+
+	inline std::ostream& operator<<(std::ostream& os, const Event& e)
+	{
+		return os << e.ToString();
+	}
 
 }
