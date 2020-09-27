dda494d3c572c749d60bcd7c7c495b6e5a323f4a
Martin <martbelko@gmail.com>
2020-09-27 14:47:00 +0200

    Added checks for memory leaks

--- Dominion/src/Dominion/Core/EntryPoint.h
+++ Dominion/src/Dominion/Core/EntryPoint.h
@@ -1,15 +1,42 @@
 #pragma once
 
+#if defined(DM_DEBUG)
+#define _CRTDBG_MAP_ALLOC // to get more details
+#include <crtdbg.h>
+#endif
+
 extern Dominion::Application* Dominion::CreateApplication();
 
 int main(int argc, char** argv)
 {
-	Dominion::Log::Init();
-	DM_CORE_INFO("Initialized engine core logger");
-	DM_INFO("Initialized client logger");
+#if defined(DM_DEBUG)
+	_CrtMemState sOld;
+	_CrtMemState sNew;
+	_CrtMemState sDiff;
+	_CrtMemCheckpoint(&sOld); // take a snapchot
+#endif
 
-	Dominion::Application* app = Dominion::CreateApplication();
-	app->Run();
+	{
+		Dominion::Log::Init();
+		DM_CORE_INFO("Initialized engine core logger");
+		DM_INFO("Initialized client logger");
+
+		Dominion::Application* app = Dominion::CreateApplication();
+		app->Run();
+	}
+
+#if defined(DM_DEBUG)
+	_CrtMemCheckpoint(&sNew); // take a snapchot
+	if (_CrtMemDifference(&sDiff, &sOld, &sNew)) // if there is a difference
+	{
+		OutputDebugString(L"-----------_CrtMemDumpStatistics ---------");
+		_CrtMemDumpStatistics(&sDiff);
+		OutputDebugString(L"-----------_CrtMemDumpAllObjectsSince ---------");
+		_CrtMemDumpAllObjectsSince(&sOld);
+		OutputDebugString(L"-----------_CrtDumpMemoryLeaks ---------");
+		_CrtDumpMemoryLeaks();
+	}
+#endif
 
 	return 0;
 }
