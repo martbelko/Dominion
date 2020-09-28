22ab7bed41efbff7392324a092b5779121ac5f16
Martin <martbelko@gmail.com>
2020-09-28 15:12:04 +0200

    Testing Layer class

--- Sandbox/src/Sandbox.cpp
+++ Sandbox/src/Sandbox.cpp
@@ -1,11 +1,30 @@
 #include <Dominion.h>
 
+class ExampleLayer : public Dominion::Layer
+{
+public:
+	ExampleLayer()
+		: Layer("Example")
+	{
+	}
+
+	void OnUpdate() override
+	{
+		DM_INFO("ExampleLayer Update");
+	}
+
+	void OnEvent(Dominion::Event& e) override
+	{
+		DM_TRACE(e);
+	}
+};
+
 class Sandbox : public Dominion::Application
 {
 public:
 	Sandbox()
 	{
-
+		PushLayer(new ExampleLayer());
 	}
 
 	~Sandbox()
