4cd9765177bfa4d68eaec1333f2b1cf0771283cc
Martin <martbelko@gmail.com>
2020-09-28 15:06:49 +0200

    Added LayerStack class

---
+++ Dominion/src/Dominion/Core/LayerStack.cpp
@@ -0,0 +1,86 @@
+#include "dmpch.h"
+#include "LayerStack.h"
+
+namespace Dominion {
+
+	LayerStack::~LayerStack()
+	{
+		for (Layer* layer : m_Layers)
+		{
+			layer->OnDetach();
+			delete layer;
+		}
+	}
+
+	void LayerStack::PushLayer(Layer* layer)
+	{
+		m_Layers.emplace(m_Layers.begin() + m_LayerInsertIndex++, layer);
+	}
+
+	void LayerStack::PushOverlay(Layer* overlay)
+	{
+		m_Layers.emplace_back(overlay);
+	}
+
+	void LayerStack::PopLayer(Layer* layer)
+	{
+		auto it = std::find(m_Layers.begin(), m_Layers.begin() + m_LayerInsertIndex, layer);
+		if (it != m_Layers.begin() + m_LayerInsertIndex)
+		{
+			layer->OnDetach();
+			m_Layers.erase(it);
+			m_LayerInsertIndex--;
+		}
+	}
+
+	void LayerStack::PopOverlay(Layer* overlay)
+	{
+		auto it = std::find(m_Layers.begin() + m_LayerInsertIndex, m_Layers.end(), overlay);
+		if (it != m_Layers.end())
+		{
+			overlay->OnDetach();
+			m_Layers.erase(it);
+		}
+	}
+
+	std::vector<Layer*>::iterator LayerStack::begin()
+	{
+		return m_Layers.begin();
+	}
+
+	std::vector<Layer*>::iterator LayerStack::end()
+	{
+		return m_Layers.end();
+	}
+
+	std::vector<Layer*>::reverse_iterator LayerStack::rbegin()
+	{
+		return m_Layers.rbegin();
+	}
+
+	std::vector<Layer*>::reverse_iterator LayerStack::rend()
+	{
+		return m_Layers.rend();
+	}
+
+	std::vector<Layer*>::const_iterator LayerStack::begin() const
+	{
+		return m_Layers.begin();
+	}
+
+	std::vector<Layer*>::const_iterator LayerStack::end() const
+	{
+		return m_Layers.end();
+	}
+
+	std::vector<Layer*>::const_reverse_iterator LayerStack::rbegin() const
+	{
+		return m_Layers.rbegin();
+	}
+
+	std::vector<Layer*>::const_reverse_iterator LayerStack::rend() const
+	{
+		return m_Layers.rend();
+	}
+
+}
