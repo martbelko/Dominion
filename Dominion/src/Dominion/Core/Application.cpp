e3f377e969306bf70bc7a06c99ae16d5c79ad6a6
Martin <martbelko@gmail.com>
2020-09-29 01:30:01 +0200

    Fixed typo, calling OnAttach on layers when pushed

--- Dominion/src/Dominion/Core/Application.cpp
+++ Dominion/src/Dominion/Core/Application.cpp
@@ -60,11 +60,13 @@ namespace Dominion {
 	void Application::PushLayer(Layer* layer)
 	{
 		m_LayerStack.PushLayer(layer);
+		layer->OnAttach();
 	}
 
 	void Application::PushOverlay(Layer* overlay)
 	{
-		m_LayerStack.PopOverlay(overlay);
+		m_LayerStack.PushOverlay(overlay);
+		overlay->OnAttach();
 	}
 
 	Dominion::Window& Application::GetWindow() const
