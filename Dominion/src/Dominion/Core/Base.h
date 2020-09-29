a45742b6009117b3b6060b92d43ef6007ca5e09d
Martin <martbelko@gmail.com>
2020-09-29 23:07:07 +0200

    Moved memory leak checks to Base.h

--- Dominion/src/Dominion/Core/Base.h
+++ Dominion/src/Dominion/Core/Base.h
@@ -1,5 +1,14 @@
 #pragma once
 
+
+#if defined(DM_DEBUG)
+	#define _CRTDBG_MAP_ALLOC
+	#include <stdlib.h>
+	#include <crtdbg.h>
+	#define DEBUG_NEW new(_NORMAL_BLOCK, __FILE__, __LINE__)
+	#define new DEBUG_NEW
+#endif
+
 #ifdef DM_DEBUG
 	#if defined(DM_PLATFORM_WINDOWS)
 		#define DM_DEBUGBREAK() __debugbreak();
