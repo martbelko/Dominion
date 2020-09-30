e5fc30b31557ba7db27b66b9a2bbe1336e77522f
Martin <martbelko@gmail.com>
2020-09-30 21:08:00 +0200

    Added RenderCommand class

---
+++ Dominion/src/Dominion/Renderer/RenderCommand.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "Dominion/Renderer/RendererAPI.h"
+
+#include <memory>
+
+namespace Dominion {
+
+	class RenderCommand
+	{
+	public:
+		static void Init()
+		{
+			s_RendererAPI->Init();
+		}
+
+		static void SetClearColor(float r, float g, float b, float a = 1.0f)
+		{
+			s_RendererAPI->SetClearColor(r, g, b, a);
+		}
+
+		static void Clear()
+		{
+			s_RendererAPI->Clear();
+		}
+	private:
+		static std::unique_ptr<RendererAPI> s_RendererAPI;
+	};
+
+	std::unique_ptr<RendererAPI> RenderCommand::s_RendererAPI = RendererAPI::Create();
+
+}
