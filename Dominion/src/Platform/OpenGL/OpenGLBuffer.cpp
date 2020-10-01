26da4adadd2024dd3065b437f626a91479fde4a3
Martin <martbelko@gmail.com>
2020-10-01 15:12:34 +0200

    Added OpenGLVertexBuffer and OpenGLIndexBuffer class

---
+++ Dominion/src/Platform/OpenGL/OpenGLBuffer.cpp
@@ -0,0 +1,51 @@
+#include "dmpch.h"
+#include "OpenGLBuffer.h"
+
+#include <glad/glad.h>
+
+namespace Dominion {
+
+	OpenGLVertexBuffer::OpenGLVertexBuffer(const float* vertices, size_t size)
+	{
+		glCreateBuffers(1, &m_RendererID);
+		glBindBuffer(GL_ARRAY_BUFFER, m_RendererID);
+		glBufferData(GL_ARRAY_BUFFER, size, vertices, GL_STATIC_DRAW);
+	}
+
+	OpenGLVertexBuffer::~OpenGLVertexBuffer()
+	{
+		glDeleteBuffers(1, &m_RendererID);
+	}
+
+	void OpenGLVertexBuffer::Bind()
+	{
+		glBindBuffer(GL_ARRAY_BUFFER, m_RendererID);
+	}
+
+	OpenGLIndexBuffer::OpenGLIndexBuffer(const uint32_t* indices, unsigned int count)
+		: m_Count(count)
+	{
+		glCreateBuffers(1, &m_RendererID);
+
+		// GL_ELEMENT_ARRAY_BUFFER is not valid without an actively bound VAO
+		// Binding with GL_ARRAY_BUFFER allows the data to be loaded regardless of VAO state.
+		glBindBuffer(GL_ARRAY_BUFFER, m_RendererID);
+		glBufferData(GL_ARRAY_BUFFER, count * sizeof(uint32_t), indices, GL_STATIC_DRAW);
+	}
+
+	OpenGLIndexBuffer::~OpenGLIndexBuffer()
+	{
+		glDeleteBuffers(1, &m_RendererID);
+	}
+
+	void OpenGLIndexBuffer::Bind()
+	{
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_RendererID);
+	}
+
+	unsigned int OpenGLIndexBuffer::GetCount() const
+	{
+		return m_Count;
+	}
+
+}
---
+++ Dominion/src/Platform/OpenGL/OpenGLBuffer.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "Dominion/Renderer/Buffer.h"
+
+namespace Dominion {
+
+	class OpenGLVertexBuffer : public VertexBuffer
+	{
+	public:
+		OpenGLVertexBuffer(const float* vertices, size_t size);
+		virtual ~OpenGLVertexBuffer() override;
+
+		virtual void Bind() override;
+	private:
+		unsigned int m_RendererID;
+	};
+
+	class OpenGLIndexBuffer : public IndexBuffer
+	{
+	public:
+		OpenGLIndexBuffer(const uint32_t* indices, unsigned int count);
+		virtual ~OpenGLIndexBuffer() override;
+
+		virtual void Bind() override;
+
+		virtual unsigned int GetCount() const override;
+	private:
+		unsigned int m_RendererID;
+		unsigned int m_Count;
+	};
+
+}
