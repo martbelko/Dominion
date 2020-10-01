ce1fc85cb0492f31f601d77b5b0ee8665439b611
Martin <martbelko@gmail.com>
2020-10-01 17:15:49 +0200

    Added one more shader

---
+++ Sandbox/TestVS2.glsl
@@ -0,0 +1,12 @@
+#version 330 core
+
+layout(location = 0) in vec3 a_Position;
+layout(location = 1) in vec4 a_Color;
+
+out vec4 v_Color;
+
+void main()
+{
+	v_Color = a_Color;
+	gl_Position = vec4(a_Position + 0.5, 1.0);
+}
