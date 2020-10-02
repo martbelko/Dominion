bce26681e673a0122d716f4363220fb8385559a9
Martin <martbelko@gmail.com>
2020-10-02 19:35:16 +0200

    Added assert for unknown renderer api

--- Dominion/src/Dominion/Renderer/InputLayout.cpp
+++ Dominion/src/Dominion/Renderer/InputLayout.cpp
@@ -23,6 +23,9 @@ namespace Dominion {
 			case RendererAPI::API::None: DM_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
 			case RendererAPI::API::OpenGL: return std::make_shared<OpenGLInputLayout>(elements, offset);
 		}
+
+		DM_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
 	}
 
 }
