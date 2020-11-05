b7d94e47fd14b3188b55e96bb9c9fe8c41b4c90f
Martin <martbelko@gmail.com>
2020-11-06 00:02:50 +0100

    Added RendererAPI check and return correct shader

--- Dominion/src/Dominion/Renderer/Shader.cpp
+++ Dominion/src/Dominion/Renderer/Shader.cpp
@@ -1,13 +1,21 @@
 #include "dmpch.h"
 #include "Shader.h"
 
+#include "Dominion/Renderer/Renderer.h"
 #include "Platform/OpenGL/OpenGLShader.h"
 
 namespace Dominion {
 
 	Ref<Shader> Shader::Create(const std::string_view& name, const std::string_view& vertexFilepath, const std::string_view& pixelFilepath)
 	{
-		return CreateRef<OpenGLShader>(name, vertexFilepath, pixelFilepath);
+		switch (Renderer::GetAPI())
+		{
+			case RendererAPI::API::None:    DM_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+			case RendererAPI::API::OpenGL:  return CreateRef<OpenGLShader>(name, vertexFilepath, pixelFilepath);
+		}
+
+		DM_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
 	}
 
 }
