e039d88a8b3f6edeb5efed4c650294474c92cbbb
Martin <martbelko@gmail.com>
2020-11-05 17:55:47 +0100

    Moved to sandbox

--- Sandbox/src/Sandbox.cpp
+++ Sandbox/src/Sandbox.cpp
@@ -14,11 +14,53 @@ public:
 	ExampleLayer()
 		: Layer("Example")
 	{
+		float vertices[] = {
+				-0.5f, -0.5f, 0.0f,
+				 0.5f, -0.5f, 0.0f,
+				 0.0f,  0.5f, 0.0f
+		};
+
+		unsigned int indices[] = {
+			0, 1, 2
+		};
+
+		m_Pipeline = Dominion::Pipeline::Create(Dominion::VertexBuffer::Create(vertices, sizeof(vertices)), Dominion::IndexBuffer::Create(indices, sizeof(indices) / sizeof(unsigned int)), Dominion::InputLayout::Create(
+			{
+				{ "Position", Dominion::DataType::Float3 }
+			}
+		));
+
+		m_Shader = Dominion::Shader::Create("Test", "TestVS.glsl", "TestPS.glsl");
+
+		float vertices2[] = {
+			-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
+			 0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,
+			 0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f
+		};
+
+		unsigned int indices2[] = {
+			0, 1, 2
+		};
+
+		m_Pipeline2 = Dominion::Pipeline::Create(Dominion::VertexBuffer::Create(vertices2, sizeof(vertices2)), Dominion::IndexBuffer::Create(indices2, sizeof(indices2) / sizeof(unsigned int)), Dominion::InputLayout::Create(
+			{
+				{ "Position", Dominion::DataType::Float3 },
+				{ "Color", Dominion::DataType::Float4 }
+			}
+		));
+
+		m_Shader2 = Dominion::Shader::Create("Test2", "TestVS2.glsl", "TestPS2.glsl");
 	}
 
 	void OnUpdate() override
 	{
+		Dominion::RenderCommand::SetClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		Dominion::RenderCommand::Clear();
 
+		Dominion::Renderer::BeginScene(m_Camera);
+		Dominion::Renderer::Submit(m_Shader, m_Pipeline);
+		Dominion::Renderer::Submit(m_Shader2, m_Pipeline2);
+		Dominion::Renderer::EndScene();
 	}
 
 	void OnEvent(Dominion::Event& e) override
@@ -31,6 +73,14 @@ public:
 		static bool show = true;
 		ImGui::ShowDemoWindow(&show);
 	}
+private:
+	Dominion::Ref<Dominion::Pipeline> m_Pipeline;
+	Dominion::Ref<Dominion::Shader> m_Shader;
+
+	Dominion::Ref<Dominion::Pipeline> m_Pipeline2;
+	Dominion::Ref<Dominion::Shader> m_Shader2;
+
+	Dominion::OrthographicCamera m_Camera = Dominion::OrthographicCamera(-1.0f, 1.0f, -1.0f, 1.0f);
 };
 
 class Sandbox : public Dominion::Application
