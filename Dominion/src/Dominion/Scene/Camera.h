0e019200563fe6c4d6ba039bc6122b09fdacb574
Martin <martbelko@gmail.com>
2020-11-06 09:15:04 +0100

    Added Refresh and OnUpdate methods

--- Dominion/src/Dominion/Scene/Camera.h
+++ Dominion/src/Dominion/Scene/Camera.h
@@ -3,6 +3,7 @@
 #include "Dominion/Events/Event.h"
 #include "Dominion/Events/WindowEvent.h"
 #include "Dominion/Events/MouseEvent.h"
+#include "Dominion/Core/Timestep.h"
 
 #include <glm/glm.hpp>
 
@@ -13,7 +14,10 @@ namespace Dominion {
 	public:
 		virtual ~Camera() = default;
 
+		virtual void OnUpdate(const Timestep& timestep) { };
 		virtual void OnEvent(Event& event) { }
+
+		virtual void Refresh() = 0;
 
 		virtual const glm::mat4& GetProjectionMatrix() const = 0;
 		virtual const glm::mat4& GetViewMatrix() const = 0;
@@ -34,7 +38,10 @@ namespace Dominion {
 
 		void SetProjection(float left, float right, float bottom, float top);
 
+		virtual void OnUpdate(const Timestep& timestep) override;
 		virtual void OnEvent(Event& e) override;
+
+		virtual void Refresh() override;
 
 		virtual const glm::vec3& GetPosition() const override;
 		virtual void SetPosition(const glm::vec3& position) override;
