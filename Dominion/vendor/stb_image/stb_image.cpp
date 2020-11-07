1305278606fe0b2da50c7f1fd4d938ab4926ff56
Martin <martbelko@gmail.com>
2020-11-07 20:45:11 +0100

    Added stb_image

---
+++ Dominion/vendor/stb_image/stb_image.cpp
@@ -0,0 +1,5 @@
+#include "dmpch.h"
+#pragma warning(disable : 6011 6262 23182 6308 26451 28182 26819) // Disable warnings from stb_image
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#pragma warning(default : 6011 6262 23182 6308 26451 28182 26819) // Enable warnings to default
