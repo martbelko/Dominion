9f5a07265e298141ee5a000294f4c77489d84c4b
Martin <martbelko@gmail.com>
2020-11-07 21:01:55 +0100

    Implemented the rest of OpenGLTexture2D

--- Dominion/src/Platform/OpenGL/OpenGLTexture.cpp
+++ Dominion/src/Platform/OpenGL/OpenGLTexture.cpp
@@ -1,18 +1,37 @@
 #include "dmpch.h"
 #include "OpenGLTexture.h"
 
+#include <stb_image.h>
 #include <glad/glad.h>
 
 namespace Dominion {
 
 	OpenGLTexture2D::OpenGLTexture2D(const std::string_view& path)
 	{
+		int width, height, nChannels;
+		stbi_set_flip_vertically_on_load(1);
+		stbi_uc* data = stbi_load(path.data(), &width, &height, &nChannels, 0);
+		DM_CORE_ASSERT(data, "Failed to load image!");
+		m_Width = width;
+		m_Height = height;
 
+		glCreateTextures(GL_TEXTURE_2D, 1, &m_RendererID);
+		glTextureStorage2D(m_RendererID, 1, GL_RGB8, m_Width, m_Height);
+
+		glTextureParameteri(m_RendererID, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTextureParameteri(m_RendererID, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+		glTextureParameteri(m_RendererID, GL_TEXTURE_WRAP_R, GL_REPEAT);
+		glTextureParameteri(m_RendererID, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+		glTextureSubImage2D(m_RendererID, 0, 0, 0, m_Width, m_Height, GL_RGB, GL_UNSIGNED_BYTE, data);
+
+		stbi_image_free(data);
 	}
 
 	OpenGLTexture2D::~OpenGLTexture2D()
 	{
-
+		glDeleteTextures(1, &m_RendererID);
 	}
 
 	uint32_t OpenGLTexture2D::GetWidth() const
@@ -27,7 +46,12 @@ namespace Dominion {
 
 	void OpenGLTexture2D::Bind() const
 	{
-		glBindTexture(GL_TEXTURE_2D, m_RendererID);
+		glBindTextureUnit(0, m_RendererID);
+	}
+
+	void OpenGLTexture2D::Bind(uint32_t slot) const
+	{
+		glBindTextureUnit(slot, m_RendererID);
 	}
 
 }
