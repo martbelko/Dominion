dd3a1e39da4349796f35cf46d266060c02f73750
Martin <martbelko@gmail.com>
2020-11-08 17:12:32 +0100

    Added debug reporting

--- Dominion/src/Platform/OpenGL/OpenGLRendererAPI.cpp
+++ Dominion/src/Platform/OpenGL/OpenGLRendererAPI.cpp
@@ -5,8 +5,36 @@
 
 namespace Dominion {
 
+	void OpenGLMessageCallback(
+		unsigned source,
+		unsigned type,
+		unsigned id,
+		unsigned severity,
+		int length,
+		const char* message,
+		const void* userParam)
+	{
+		switch (severity)
+		{
+			case GL_DEBUG_SEVERITY_HIGH:         DM_CORE_FATAL(message); return;
+			case GL_DEBUG_SEVERITY_MEDIUM:       DM_CORE_ERROR(message); return;
+			case GL_DEBUG_SEVERITY_LOW:          DM_CORE_WARN(message); return;
+			case GL_DEBUG_SEVERITY_NOTIFICATION: DM_CORE_TRACE(message); return;
+		}
+
+		DM_CORE_ASSERT(false, "Unknown severity level!");
+	}
+
 	void OpenGLRendererAPI::Init()
 	{
+	#ifdef DM_DEBUG
+		glEnable(GL_DEBUG_OUTPUT);
+		glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+		glDebugMessageCallback(OpenGLMessageCallback, nullptr);
+
+		glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DEBUG_SEVERITY_NOTIFICATION, 0, NULL, GL_FALSE);
+	#endif
+
 		glEnable(GL_BLEND);
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	}
