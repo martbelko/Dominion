2ee53f6786498379d57b7135d4251b79eaeae961
Martin <martbelko@gmail.com>
2020-11-08 16:40:19 +0100

    Added info to output, assert if not supported version

--- Dominion/src/Platform/OpenGL/OpenGLContext.cpp
+++ Dominion/src/Platform/OpenGL/OpenGLContext.cpp
@@ -18,6 +18,13 @@ namespace Dominion {
 		glfwMakeContextCurrent(m_WindowHandle);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		DM_CORE_ASSERT(status, "Failed to initialize Glad!");
+
+		DM_CORE_INFO("OpenGL Info:");
+		DM_CORE_INFO("  Vendor: {0}", glGetString(GL_VENDOR));
+		DM_CORE_INFO("  Renderer: {0}", glGetString(GL_RENDERER));
+		DM_CORE_INFO("  Version: {0}", glGetString(GL_VERSION));
+
+		DM_CORE_ASSERT(GLVersion.major > 4 || (GLVersion.major == 4 && GLVersion.minor >= 5), "Dominion requires at least OpenGL version 4.5!");
 	}
 
 	void OpenGLContext::SwapBuffers()
