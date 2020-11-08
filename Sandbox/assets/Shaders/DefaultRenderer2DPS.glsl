fc79df09aa23d1c035fc3a7062ec035b5d332ff7
Martin <martbelko@gmail.com>
2020-11-08 19:58:52 +0100

    Added texture support

--- Sandbox/assets/Shaders/DefaultRenderer2DPS.glsl
+++ Sandbox/assets/Shaders/DefaultRenderer2DPS.glsl
@@ -5,8 +5,9 @@ out vec4 color;
 in vec2 v_TexCoord;
 
 uniform vec4 u_Color;
+uniform sampler2D u_Texture;
 
 void main()
 {
-	color = u_Color;
+	color = texture(u_Texture, v_TexCoord) * u_Color;
 }
