72a1001e88932f960073d8b99e98445f07b3b22f
Martin <martbelko@gmail.com>
2020-11-08 22:30:07 +0100

    Fixed compilation bugs, moved profiler switch to Base.h

--- Dominion/src/Dominion/Debug/Instrumentor.h
+++ Dominion/src/Dominion/Debug/Instrumentor.h
@@ -9,6 +9,7 @@
 #include <mutex>
 #include <sstream>
 
+#include "Dominion/Core/Base.h"
 #include "Dominion/Core/Log.h"
 
 namespace Dominion {
@@ -202,39 +203,38 @@ namespace Dominion {
 	}
 }
 
-#define DM_PROFILE 0
 #if DM_PROFILE
 // Resolve which function signature macro will be used. Note that this only
 // is resolved when the (pre)compiler starts, so the syntax highlighting
 // could mark the wrong one in your editor!
 #if defined(__GNUC__) || (defined(__MWERKS__) && (__MWERKS__ >= 0x3000)) || (defined(__ICC) && (__ICC >= 600)) || defined(__ghs__)
-#define DM_FUNC_SIG __PRETTY_FUNCTION__
+	#define DM_FUNC_SIG __PRETTY_FUNCTION__
 #elif defined(__DMC__) && (__DMC__ >= 0x810)
-#define DM_FUNC_SIG __PRETTY_FUNCTION__
+	#define DM_FUNC_SIG __PRETTY_FUNCTION__
 #elif (defined(__FUNCSIG__) || (_MSC_VER))
-#define DM_FUNC_SIG __FUNCSIG__
+	#define DM_FUNC_SIG __FUNCSIG__
 #elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
-#define DM_FUNC_SIG __FUNCTION__
+	#define DM_FUNC_SIG __FUNCTION__
 #elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x550)
-#define DM_FUNC_SIG __FUNC__
+	#define DM_FUNC_SIG __FUNC__
 #elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)
-#define DM_FUNC_SIG __func__
+	#define DM_FUNC_SIG __func__
 #elif defined(__cplusplus) && (__cplusplus >= 201103)
-#define DM_FUNC_SIG __func__
+	#define DM_FUNC_SIG __func__
 #else
-#define DM_FUNC_SIG "HZ_FUNC_SIG unknown!"
+	#define DM_FUNC_SIG "DM_FUNC_SIG unknown!"
 #endif
 
-#define DM_PROFILE_BEGIN_SESSION(name, filepath) ::Hazel::Instrumentor::Get().BeginSession(name, filepath)
-#define DM_PROFILE_END_SESSION() ::Hazel::Instrumentor::Get().EndSession()
-#define DM_PROFILE_SCOPE_LINE2(name, line) constexpr auto fixedName##line = ::Hazel::InstrumentorUtils::CleanupOutputString(name, "__cdecl ");\
-											   ::Hazel::InstrumentationTimer timer##line(fixedName##line.Data)
-#define DM_PROFILE_SCOPE_LINE(name, line) HZ_PROFILE_SCOPE_LINE2(name, line)
-#define DM_PROFILE_SCOPE(name) HZ_PROFILE_SCOPE_LINE(name, __LINE__)
-#define DM_PROFILE_FUNCTION() HZ_PROFILE_SCOPE(HZ_FUNC_SIG)
+	#define DM_PROFILE_BEGIN_SESSION(name, filepath) ::Dominion::Instrumentor::Get().BeginSession(name, filepath)
+	#define DM_PROFILE_END_SESSION() ::Dominion::Instrumentor::Get().EndSession()
+	#define DM_PROFILE_SCOPE_LINE2(name, line) constexpr auto fixedName##line = ::Dominion::InstrumentorUtils::CleanupOutputString(name, "__cdecl ");\
+												   ::Dominion::InstrumentationTimer timer##line(fixedName##line.Data)
+	#define DM_PROFILE_SCOPE_LINE(name, line) DM_PROFILE_SCOPE_LINE2(name, line)
+	#define DM_PROFILE_SCOPE(name) DM_PROFILE_SCOPE_LINE(name, __LINE__)
+	#define DM_PROFILE_FUNCTION() DM_PROFILE_SCOPE(DM_FUNC_SIG)
 #else
-#define DM_PROFILE_BEGIN_SESSION(name, filepath)
-#define DM_PROFILE_END_SESSION()
-#define DM_PROFILE_SCOPE(name)
-#define DM_PROFILE_FUNCTION()
+	#define DM_PROFILE_BEGIN_SESSION(name, filepath)
+	#define DM_PROFILE_END_SESSION()
+	#define DM_PROFILE_SCOPE(name)
+	#define DM_PROFILE_FUNCTION()
 #endif
