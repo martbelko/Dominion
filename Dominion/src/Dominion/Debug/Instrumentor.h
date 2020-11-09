afb235c988a8bc30bdbc1bb118d82522998bd60b
Martin <martbelko@gmail.com>
2020-11-09 12:06:20 +0100

    Code refactor

--- Dominion/src/Dominion/Debug/Instrumentor.h
+++ Dominion/src/Dominion/Debug/Instrumentor.h
@@ -204,31 +204,31 @@ namespace Dominion {
 }
 
 #if DM_PROFILE
-// Resolve which function signature macro will be used. Note that this only
-// is resolved when the (pre)compiler starts, so the syntax highlighting
-// could mark the wrong one in your editor!
-#if defined(__GNUC__) || (defined(__MWERKS__) && (__MWERKS__ >= 0x3000)) || (defined(__ICC) && (__ICC >= 600)) || defined(__ghs__)
-	#define DM_FUNC_SIG __PRETTY_FUNCTION__
-#elif defined(__DMC__) && (__DMC__ >= 0x810)
-	#define DM_FUNC_SIG __PRETTY_FUNCTION__
-#elif (defined(__FUNCSIG__) || (_MSC_VER))
-	#define DM_FUNC_SIG __FUNCSIG__
-#elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
-	#define DM_FUNC_SIG __FUNCTION__
-#elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x550)
-	#define DM_FUNC_SIG __FUNC__
-#elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)
-	#define DM_FUNC_SIG __func__
-#elif defined(__cplusplus) && (__cplusplus >= 201103)
-	#define DM_FUNC_SIG __func__
-#else
-	#define DM_FUNC_SIG "DM_FUNC_SIG unknown!"
-#endif
+	// Resolve which function signature macro will be used. Note that this only
+	// is resolved when the (pre)compiler starts, so the syntax highlighting
+	// could mark the wrong one in your editor!
+	#if defined(__GNUC__) || (defined(__MWERKS__) && (__MWERKS__ >= 0x3000)) || (defined(__ICC) && (__ICC >= 600)) || defined(__ghs__)
+		#define DM_FUNC_SIG __PRETTY_FUNCTION__
+	#elif defined(__DMC__) && (__DMC__ >= 0x810)
+		#define DM_FUNC_SIG __PRETTY_FUNCTION__
+	#elif (defined(__FUNCSIG__) || (_MSC_VER))
+		#define DM_FUNC_SIG __FUNCSIG__
+	#elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
+		#define DM_FUNC_SIG __FUNCTION__
+	#elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x550)
+		#define DM_FUNC_SIG __FUNC__
+	#elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)
+		#define DM_FUNC_SIG __func__
+	#elif defined(__cplusplus) && (__cplusplus >= 201103)
+		#define DM_FUNC_SIG __func__
+	#else
+		#define DM_FUNC_SIG "DM_FUNC_SIG unknown!"
+	#endif
 
 	#define DM_PROFILE_BEGIN_SESSION(name, filepath) ::Dominion::Instrumentor::Get().BeginSession(name, filepath)
 	#define DM_PROFILE_END_SESSION() ::Dominion::Instrumentor::Get().EndSession()
 	#define DM_PROFILE_SCOPE_LINE2(name, line) constexpr auto fixedName##line = ::Dominion::InstrumentorUtils::CleanupOutputString(name, "__cdecl ");\
-												   ::Dominion::InstrumentationTimer timer##line(fixedName##line.Data)
+                                               ::Dominion::InstrumentationTimer timer##line(fixedName##line.Data)
 	#define DM_PROFILE_SCOPE_LINE(name, line) DM_PROFILE_SCOPE_LINE2(name, line)
 	#define DM_PROFILE_SCOPE(name) DM_PROFILE_SCOPE_LINE(name, __LINE__)
 	#define DM_PROFILE_FUNCTION() DM_PROFILE_SCOPE(DM_FUNC_SIG)
