c99d552185c2aa3f92033311db8adae5e5a4f6f0
Martin <martbelko@gmail.com>
2020-11-09 15:37:45 +0100

    Finished Batch rendering

--- Dominion/src/Dominion/Renderer/Renderer2D.cpp
+++ Dominion/src/Dominion/Renderer/Renderer2D.cpp
@@ -17,6 +17,8 @@ namespace Dominion {
 		glm::vec3 Position;
 		glm::vec4 Color;
 		glm::vec2 TexCoord;
+		float TexIndex;
+		float TilingFactor;
 	};
 
 
@@ -25,6 +27,7 @@ namespace Dominion {
 		const uint32_t MaxQuads = 10000;
 		const uint32_t MaxVertices = MaxQuads * 4;
 		const uint32_t MaxIndices = MaxQuads * 6;
+		static const uint32_t MaxTextureSlots = 32; // TODO: RenderCaps
 
 		Ref<VertexBuffer> QuadVertexBuffer;
 		Ref<Pipeline> QuadPipeline;
@@ -34,6 +37,9 @@ namespace Dominion {
 		uint32_t QuadIndexCount = 0;
 		QuadVertex* QuadVertexBufferBase = nullptr;
 		QuadVertex* QuadVertexBufferPtr = nullptr;
+
+		std::array<Ref<Texture2D>, MaxTextureSlots> TextureSlots;
+		uint32_t TextureSlotIndex = 1; // 0 = White Texture
 
 		glm::vec4 QuadVertexPositions[4];
 	};
@@ -71,7 +77,9 @@ namespace Dominion {
 			{
 				{ "Position", Dominion::DataType::Float3 },
 				{ "Color", Dominion::DataType::Float4 },
-				{ "TexCoord", Dominion::DataType::Float2 }
+				{ "TexCoord", Dominion::DataType::Float2 },
+				{ "TexIndex", Dominion::DataType::Float },
+				{ "TilingFactor", Dominion::DataType::Float }
 			}
 		));
 
@@ -85,6 +93,8 @@ namespace Dominion {
 		s_Data.QuadVertexPositions[1] = { 0.5f, -0.5f, 0.0f, 1.0f };
 		s_Data.QuadVertexPositions[2] = { 0.5f,  0.5f, 0.0f, 1.0f };
 		s_Data.QuadVertexPositions[3] = { -0.5f,  0.5f, 0.0f, 1.0f };
+
+		s_Data.TextureSlots[0] = s_Data.WhiteTexture;
 	}
 
 	void Renderer2D::Shutdown()
@@ -105,7 +115,7 @@ namespace Dominion {
 
 		s_Data.QuadIndexCount = 0;
 		s_Data.QuadVertexBufferPtr = s_Data.QuadVertexBufferBase;
-
+		s_Data.TextureSlotIndex = 1;
 	}
 
 	void Renderer2D::EndScene()
@@ -120,6 +130,11 @@ namespace Dominion {
 
 	void Renderer2D::Flush()
 	{
+		for (uint32_t i = 0; i < s_Data.TextureSlotIndex; ++i)
+		{
+			s_Data.TextureSlots[i]->Bind(i);
+		}
+
 		RenderCommand::DrawIndexed(s_Data.QuadPipeline, s_Data.QuadIndexCount);
 	}
 
@@ -192,6 +207,8 @@ namespace Dominion {
 			s_Data.QuadVertexBufferPtr->Position = transform * s_Data.QuadVertexPositions[i];
 			s_Data.QuadVertexBufferPtr->Color = color;
 			s_Data.QuadVertexBufferPtr->TexCoord = textureCoords[i];
+			s_Data.QuadVertexBufferPtr->TexIndex = 0.0f;
+			s_Data.QuadVertexBufferPtr->TilingFactor = 1.0f;
 			s_Data.QuadVertexBufferPtr++;
 		}
 
@@ -202,13 +219,45 @@ namespace Dominion {
 	{
 		DM_PROFILE_FUNCTION();
 
-		s_Data.TextureShader->SetMat4("u_Transform", transform);
-		s_Data.TextureShader->SetFloat4("u_Color", { 1.0f, 1.0f, 1.0f, 1.0f });
-		s_Data.TextureShader->SetFloat("u_TilingFactor", tilingFactor);
-		s_Data.TextureShader->SetInt("u_Texture", 0);
-		texture->Bind(0);
-		s_Data.QuadPipeline->Bind();
-		RenderCommand::DrawIndexed(s_Data.QuadPipeline);
+		constexpr glm::vec4 color = { 1.0f, 1.0f, 1.0f, 1.0f };
+		constexpr glm::vec2 textureCoords[] = { { 0.0f, 0.0f }, { 1.0f, 0.0f }, { 1.0f, 1.0f }, { 0.0f, 1.0f } };
+
+		float textureIndex = 0.0f;
+		for (uint32_t i = 1; i < s_Data.TextureSlotIndex; ++i)
+		{
+			if (*s_Data.TextureSlots[i].get() == *texture.get())
+			{
+				textureIndex = static_cast<float>(i);
+				break;
+			}
+		}
+
+		if (textureIndex == 0.0f)
+		{
+			textureIndex = static_cast<float>(s_Data.TextureSlotIndex);
+			s_Data.TextureSlots[s_Data.TextureSlotIndex] = texture;
+			s_Data.TextureSlotIndex++;
+		}
+
+		for (int i = 0; i < 4; ++i)
+		{
+			s_Data.QuadVertexBufferPtr->Position = transform * s_Data.QuadVertexPositions[i];
+			s_Data.QuadVertexBufferPtr->Color = color;
+			s_Data.QuadVertexBufferPtr->TexCoord = textureCoords[i];
+			s_Data.QuadVertexBufferPtr->TexIndex = textureIndex;
+			s_Data.QuadVertexBufferPtr->TilingFactor = tilingFactor;
+			s_Data.QuadVertexBufferPtr++;
+		}
+
+		s_Data.QuadIndexCount += 6;
+
+		int samplers[s_Data.MaxTextureSlots];
+		for (uint32_t i = 0; i < s_Data.MaxTextureSlots; ++i)
+		{
+			samplers[i] = i;
+		}
+
+		s_Data.TextureShader->SetIntArray("u_Texture", samplers, s_Data.MaxTextureSlots * sizeof(int));
 	}
 
 }
--- Sandbox/assets/Shaders/DefaultRenderer2DPS.glsl
+++ Sandbox/assets/Shaders/DefaultRenderer2DPS.glsl
@@ -3,12 +3,13 @@
 out vec4 color;
 
 in vec2 v_TexCoord;
+in vec4 v_Color;
+in float v_TexIndex;
+in float v_TilingFactor;
 
-uniform vec4 u_Color;
-uniform sampler2D u_Texture;
-uniform float u_TilingFactor;
+uniform sampler2D u_Texture[32];
 
 void main()
 {
-	color = texture(u_Texture, v_TexCoord * u_TilingFactor) * u_Color;
+	color = texture(u_Texture[int(v_TexIndex)], v_TexCoord * v_TilingFactor) * v_Color;
 }
