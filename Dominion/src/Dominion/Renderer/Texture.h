fb22f819d1531ebb20725a20ff620d8fb17b1934
Martin <martbelko@gmail.com>
2020-11-09 15:37:21 +0100

    Fixed compilation bug

--- Dominion/src/Dominion/Renderer/Texture.h
+++ Dominion/src/Dominion/Renderer/Texture.h
@@ -12,8 +12,6 @@ namespace Dominion {
 
 		virtual void Bind() const = 0;
 		virtual void Bind(uint32_t slot) const = 0;
-
-		virtual bool operator==(const Texture2D& other) const = 0;
 	};
 
 	class Texture2D : public Texture
@@ -25,6 +23,8 @@ namespace Dominion {
 		virtual uint32_t GetHeight() const = 0;
 
 		virtual void SetData(const void* data, uint32_t size) = 0;
+
+		virtual bool operator==(const Texture2D& other) const = 0;
 
 		static Ref<Texture2D> Create(const std::string_view& filepath);
 		static Ref<Texture2D> Create(uint32_t width, uint32_t height);
