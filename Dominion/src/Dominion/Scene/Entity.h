01a4e928d7513e03aba193c7854a623ff404174b
Martin <martbelko@gmail.com>
2020-11-12 23:19:41 +0100

    Fixed warnings

--- Dominion/src/Dominion/Scene/Entity.h
+++ Dominion/src/Dominion/Scene/Entity.h
@@ -45,7 +45,7 @@ namespace Dominion {
 		operator bool() const { return m_EntityHandle != entt::null; }
 	private:
 		entt::entity m_EntityHandle{ entt::null };
-		Scene* m_Scene;
+		Scene* m_Scene = nullptr;
 	};
 
 }
