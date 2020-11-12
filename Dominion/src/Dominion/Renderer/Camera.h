17627d38f74316943d2ef15905acb8e3beeec2d2
Martin <martbelko@gmail.com>
2020-11-12 20:44:03 +0100

    Changed private to protected

--- Dominion/src/Dominion/Renderer/Camera.h
+++ Dominion/src/Dominion/Renderer/Camera.h
@@ -14,8 +14,8 @@ namespace Dominion {
 		virtual ~Camera() = default;
 
 		const glm::mat4& GetProjection() const { return m_Projection; }
-	private:
-		glm::mat4 m_Projection{ 1.0f };
+	protected:
+		glm::mat4 m_Projection = glm::mat4(1.0f);
 	};
 
 }
