f7300e81a35007c0ba02653b8000dea326b6af0d
Martin <martbelko@gmail.com>
2020-11-13 14:16:14 +0100

    Fixed camera bug when adding new entity

--- Dominion/src/Dominion/Scene/Entity.h
+++ Dominion/src/Dominion/Scene/Entity.h
@@ -19,7 +19,9 @@ namespace Dominion {
 		T& AddComponent(Args&&... args)
 		{
 			DM_CORE_ASSERT(!HasComponent<T>(), "Entity already has this component!");
-			return m_Scene->m_Registry.emplace<T>(m_EntityHandle, std::forward<Args>(args)...);
+			T& component = m_Scene->m_Registry.emplace<T>(m_EntityHandle, std::forward<Args>(args)...);
+			m_Scene->OnComponentAdded<T>(*this, component);
+			return component;
 		}
 
 		template<typename T>
