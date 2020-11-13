6acb1879982219d06870b4ae657c4e6cec4cac2e
Martin <martbelko@gmail.com>
2020-11-13 17:33:42 +0100

    Changed minimum size of ImGui window in docking

--- Dominion-Editor/src/EditorLayer.cpp
+++ Dominion-Editor/src/EditorLayer.cpp
@@ -148,11 +148,16 @@ namespace Dominion {
 					ImGui::PopStyleVar(2);
 
 				ImGuiIO& io = ImGui::GetIO();
+				ImGuiStyle& style = ImGui::GetStyle();
+
+				float defaultWindowSizeX = style.WindowMinSize.x;
+				style.WindowMinSize.x = 370.0f;
 				if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
 				{
 					ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
 					ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
 				}
+				style.WindowMinSize.x = defaultWindowSizeX;
 
 				if (ImGui::BeginMenuBar())
 				{
