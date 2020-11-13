5ab95833edb4d39a81501334816e4950540ee8cc
Martin <martbelko@gmail.com>
2020-11-13 12:03:26 +0100

    Added imgui_internal include

--- Dominion/src/Dominion.h
+++ Dominion/src/Dominion.h
@@ -28,7 +28,9 @@
 #if defined(new)
 	#undef new
 	#include "../imgui/imgui.h"
+	#include "../imgui/imgui_internal.h"
 	#define new DEBUG_NEW
 #else
 	#include "../imgui/imgui.h"
+	#include "../imgui/imgui_internal.h"
 #endif
