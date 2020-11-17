1aa556de66394a3b1bcb2319d812257acf2695c0
Martin <martbelko@gmail.com>
2020-11-17 22:04:18 +0100

    Refactored for SimpleRenderer2D use

--- Dominion-Editor/assets/Shaders/TestPS.glsl
+++ Dominion-Editor/assets/Shaders/TestPS.glsl
@@ -1,8 +1,13 @@
 #version 330 core
 
-layout(location = 0) out vec4 color;
+in vec2 v_TexCoord;
+
+uniform vec4 u_Color;
+uniform sampler2D u_Texture;
+
+out vec4 color;
 
 void main()
 {
-	color = vec4(1.0, 0.0, 0.0, 1.0);
+	color = texture(u_Texture, v_TexCoord) * u_Color;
 }
