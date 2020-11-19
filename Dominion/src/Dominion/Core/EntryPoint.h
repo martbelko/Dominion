d2ae0d3fbf98833989a760557e7342087c3ac227
Martin <martbelko@gmail.com>
2020-11-19 13:46:27 +0100

    Redirecting CRT mem warnings to stdout

--- Dominion/src/Dominion/Core/EntryPoint.h
+++ Dominion/src/Dominion/Core/EntryPoint.h
@@ -8,6 +8,12 @@ int main(int argc, char** argv)
 {
 #if defined(DM_DEBUG)
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
+	_CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDOUT);
+	_CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_FILE);
+	_CrtSetReportFile(_CRT_ERROR, _CRTDBG_FILE_STDOUT);
+	_CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_FILE);
+	_CrtSetReportFile(_CRT_ASSERT, _CRTDBG_FILE_STDOUT);
 #endif
 
 	DM_PROFILE_BEGIN_SESSION("Startup", "DominionProfile-Startup.json");
