02db52f9f9f41d0a2b64099073daee11721553b4
Martin <martbelko@gmail.com>
2020-11-20 01:08:02 +0100

    Added DoesIntersect for checking ray intersection with quads

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -24,6 +24,52 @@ void Sandbox2D::OnAttach()
 	m_Camera.GetCamera().SetPosition(glm::vec3(0.0f, 0.0f, 10.0f));
 }
 
+bool DoesIntersectTriangle(const glm::vec3& rayOrigin, const glm::vec3& rayVector, const glm::vec3& vertex0, const glm::vec3& vertex1, const glm::vec3& vertex2)
+{
+	constexpr float epsilon = 0.0000001f;
+	glm::vec3 edge1, edge2, h, s, q;
+	float a, f, u, v;
+	edge1 = vertex1 - vertex0;
+	edge2 = vertex2 - vertex0;
+	h = glm::cross(edge2, rayVector);
+	a = glm::dot(edge1, h);
+	if (a > -epsilon && a < epsilon)
+		return false;
+
+	f = 1.0f / a;
+	s = rayOrigin - vertex0;
+	u = f * glm::dot(s, h);
+	if (u < 0.0f || u > 1.0f)
+		return false;
+
+	q = glm::cross(edge1, s);
+	v = f * glm::dot(rayVector, q);
+	if (v < 0.0f || u + v > 1.0f)
+		return false;
+	float t = f * glm::dot(edge2, q);
+	if (t > epsilon)
+		return true;
+	else
+		return false;
+}
+
+bool DoesIntersect(const glm::vec3& rayOrigin, const glm::vec3& rayVector, const glm::vec3& quadPos, const glm::vec2& quadSize)
+{
+	float left = quadPos.x - quadSize.x / 2.0f;
+	float top = quadPos.y + quadSize.y / 2.0f;
+	float right = quadPos.x + quadSize.x / 2.0f;
+	float bottom = quadPos.y - quadSize.y / 2.0f;
+
+	// Check left triangle
+	glm::vec3 vertex0 = glm::vec3(left, bottom, quadPos.z);
+	glm::vec3 vertex1 = glm::vec3(right, top, quadPos.z);
+	glm::vec3 vertex2 = glm::vec3(left, top, quadPos.z);
+	glm::vec3 vertex3 = glm::vec3(right, bottom, quadPos.z);
+
+	return DoesIntersectTriangle(rayOrigin, rayVector, vertex0, vertex1, vertex2)
+		|| DoesIntersectTriangle(rayOrigin, rayVector, vertex0, vertex3, vertex1);
+}
+
 void Sandbox2D::OnUpdate(const Dominion::Timestep& timestep)
 {
 	DM_PROFILE_FUNCTION();
