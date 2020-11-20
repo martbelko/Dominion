cc719ce332070237367d877c3535631004de239f
Martin <martbelko@gmail.com>
2020-11-20 21:30:46 +0100

    Added virtual Draw() method into RendererAPI class

--- Dominion/src/Dominion/Renderer/RendererAPI.h
+++ Dominion/src/Dominion/Renderer/RendererAPI.h
@@ -22,6 +22,7 @@ namespace Dominion {
 		virtual void SetClearColor(float r, float g, float b, float a) = 0;
 		virtual void Clear() = 0;
 		virtual void DrawIndexed(const Ref<Pipeline> pipeline, uint32_t count) = 0;
+		virtual void Draw(const Ref<Pipeline> pipeline, uint32_t count) = 0;
 
 		static API GetAPI() { return s_API; }
 		static Scope<RendererAPI> Create();
