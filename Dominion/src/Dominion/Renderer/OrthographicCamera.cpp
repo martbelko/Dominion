8943aea8e6c3d7061f2b61a33b32ca0ff323f902
Martin <martbelko@gmail.com>
2020-12-14 23:42:51 +0100

    Small code changes

--- Dominion/src/Dominion/Renderer/OrthographicCamera.cpp
+++ Dominion/src/Dominion/Renderer/OrthographicCamera.cpp
@@ -5,19 +5,23 @@
 
 namespace Dominion {
 
-	OrthographicCamera::OrthographicCamera(F32 left, F32 right, F32 bottom, F32 top)
-		: m_ProjectionMatrix(glm::ortho(left, right, bottom, top, -1.0f, 1.0f)), m_ViewMatrix(1.0f)
+	OrthographicCamera::OrthographicCamera(F32 left, F32 right, F32 bottom, F32 top, F32 size)
+		: m_ProjectionMatrix(glm::ortho(left * size, right * size, bottom * size, top * size, -1.0f, 1.0f)), m_ViewMatrix(1.0f),
+		m_ViewProjectionMatrix(m_ProjectionMatrix * m_ViewMatrix), m_Size(size)
 	{
 		DM_PROFILE_FUNCTION();
-
-		m_ViewProjectionMatrix = m_ProjectionMatrix * m_ViewMatrix;
 	}
 
 	void OrthographicCamera::SetProjection(F32 left, F32 right, F32 bottom, F32 top)
 	{
+		SetProjection(left, right, bottom, top, m_Size);
+	}
+
+	void OrthographicCamera::SetProjection(F32 left, F32 right, F32 bottom, F32 top, F32 size)
+	{
 		DM_PROFILE_FUNCTION();
 
-		m_ProjectionMatrix = glm::ortho(left, right, bottom, top, -1.0f, 1.0f);
+		m_ProjectionMatrix = glm::ortho(left * size, right * size, bottom * size, top * size, -1.0f, 1.0f);
 		m_ViewProjectionMatrix = m_ProjectionMatrix * m_ViewMatrix;
 	}
 
@@ -26,7 +30,7 @@ namespace Dominion {
 		DM_PROFILE_FUNCTION();
 
 		glm::mat4 transform = glm::translate(glm::mat4(1.0f), m_Position) *
-			glm::rotate(glm::mat4(1.0f), glm::radians(m_Rotation), glm::vec3(0, 0, 1));
+			glm::rotate(glm::mat4(1.0f), m_Rotation, glm::vec3(0, 0, 1));
 
 		m_ViewMatrix = glm::inverse(transform);
 		m_ViewProjectionMatrix = m_ProjectionMatrix * m_ViewMatrix;
--- Dominion/src/Dominion/Renderer/OrthographicCamera.h
+++ Dominion/src/Dominion/Renderer/OrthographicCamera.h
@@ -9,9 +9,10 @@ namespace Dominion {
 	class OrthographicCamera
 	{
 	public:
-		OrthographicCamera(F32 left = -1.0f, F32 right = 1.0f, F32 bottom = -1.0f, F32 top = 1.0f);
+		OrthographicCamera(F32 left = -1.0f, F32 right = 1.0f, F32 bottom = -1.0f, F32 top = 1.0f, F32 size = 1.0f);
 
 		void SetProjection(F32 left, F32 right, F32 bottom, F32 top);
+		void SetProjection(F32 left, F32 right, F32 bottom, F32 top, F32 size);
 
 		const glm::vec3& GetPosition() const { return m_Position; }
 		glm::vec3& GetPosition() { return m_Position; }
@@ -20,6 +21,8 @@ namespace Dominion {
 		F32 GetRotation() const { return m_Rotation; }
 		F32& GetRotation() { return m_Rotation; }
 		void SetRotation(F32 rotation) { m_Rotation = rotation; RecalculateViewMatrix(); }
+
+		void SetPositionAndRotation(const glm::vec3& position, F32 rotationInRadians) { m_Position = position, m_Rotation = rotationInRadians; RecalculateViewMatrix(); }
 
 		const glm::mat4& GetProjectionMatrix() const { return m_ProjectionMatrix; }
 		const glm::mat4& GetViewMatrix() const { return m_ViewMatrix; }
@@ -32,7 +35,8 @@ namespace Dominion {
 		glm::mat4 m_ViewProjectionMatrix;
 
 		glm::vec3 m_Position = { 0.0f, 0.0f, 0.0f };
-		F32 m_Rotation = 0.0f; // In radians
+		F32 m_Rotation = 0.0f; // In radians, around Z-axis
+		F32 m_Size;
 
 		friend class OrthographicCameraController;
 	};
