2ea7dd141fa9cfcce9203542abec922396f38cdd
Martin <martbelko@gmail.com>
2021-01-08 22:40:38 +0100

    Changed back to C-like struct

--- Dominion/src/Dominion/Scene/Components/ColliderComponent.h
+++ Dominion/src/Dominion/Scene/Components/ColliderComponent.h
@@ -1,27 +1,17 @@
 #pragma once
 
+#include "Dominion/Core/Base.h"
 #include "Dominion/Core/Physics.h"
 
 #include <glm/glm.hpp>
 
 namespace Dominion {
 
-	class BoxCollider2DComponent
+	struct BoxCollider2DComponent
 	{
-	public:
-		const glm::vec2& GetCenter() const { return m_Center; }
-		const glm::vec2& GetScale() const { return m_Scale; }
-		physx::PxMaterial* GetPhysicsMaterial() const { return m_PhysicsMaterial; }
-		physx::PxRigidDynamic* GetPhysicsActor() const { return m_PhysicsActor; }
-		bool IsTrigger() const { return m_IsTrigger; }
-
-
-	private:
-		glm::vec2 m_Center{ 0.0f, 0.0f };
-		glm::vec2 m_Scale{ 1.0f, 1.0f };
-		physx::PxMaterial* m_PhysicsMaterial = Physics::GetPhysXPhysics()->createMaterial(0.5f, 0.5f, 0.1f);
-		physx::PxRigidDynamic* m_PhysicsActor = nullptr;
-		bool m_IsTrigger = false;
+		glm::vec2 centerOffset{ 0.0f, 0.0f };
+		physx::PxMaterial* physicsMaterial = Physics::GetPhysXPhysics()->createMaterial(0.5f, 0.5f, 0.1f);
+		physx::PxRigidActor* physicsActor = nullptr;
 	};
 
 }
