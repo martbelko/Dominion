8fbe48db70c2480d52ea1eac6c7329a280c06b6d
Martin <martbelko@gmail.com>
2021-10-13 21:13:07 +0200

    Added TODO

--- Dominion/src/Dominion/Core/Layer.h
+++ Dominion/src/Dominion/Core/Layer.h
@@ -12,6 +12,8 @@ namespace Dominion {
 		Layer(const std::string_view& debugName = "Layer") : m_DebugName(debugName) {}
 		virtual ~Layer() = default;
 
+		// TODO: Rule of 5
+
 		virtual void OnAttach() {}
 		virtual void OnDetach() {}
 		virtual void OnUpdate(const Timestep& timestep) {}
