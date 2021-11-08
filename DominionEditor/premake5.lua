b450bb79760eb402b2dd5c45635008d15c43b706
Martin Belko <martbelko@gmail.com>
2021-11-08 21:05:20 +0100

    Updated premake

--- DominionEditor/premake5.lua
+++ DominionEditor/premake5.lua
@@ -32,3 +32,27 @@ project "DominionEditor"
 		{
 			"{COPYDIR} \"%{LibraryDir.VulkanSDK_DebugDLL}\" \"%{cfg.targetdir}\""
 		}
+
+	filter "configurations:PGO"
+		postbuildcommands
+		{
+			"{COPYDIR} \"%{LibraryDir.VulkanSDK}/shaderc_shared.lib\" \"%{cfg.targetdir}\"",
+			"{COPYDIR} \"%{LibraryDir.VulkanSDK}/spirv-cross-c-shared.lib\" \"%{cfg.targetdir}\"",
+			"{COPYDIR} \"%{LibraryDir.VulkanSDK}/SPIRV-Tools-shared.lib\" \"%{cfg.targetdir}\""
+		}
+
+	filter "configurations:Release"
+		postbuildcommands
+		{
+			"{COPYDIR} \"%{LibraryDir.VulkanSDK}/shaderc_shared.lib\" \"%{cfg.targetdir}\"",
+			"{COPYDIR} \"%{LibraryDir.VulkanSDK}/spirv-cross-c-shared.lib\" \"%{cfg.targetdir}\"",
+			"{COPYDIR} \"%{LibraryDir.VulkanSDK}/SPIRV-Tools-shared.lib\" \"%{cfg.targetdir}\""
+		}
+
+	filter "configurations:Dist"
+		postbuildcommands
+		{
+			"{COPYDIR} \"%{LibraryDir.VulkanSDK}/shaderc_shared.lib\" \"%{cfg.targetdir}\"",
+			"{COPYDIR} \"%{LibraryDir.VulkanSDK}/spirv-cross-c-shared.lib\" \"%{cfg.targetdir}\"",
+			"{COPYDIR} \"%{LibraryDir.VulkanSDK}/SPIRV-Tools-shared.lib\" \"%{cfg.targetdir}\""
+		}
