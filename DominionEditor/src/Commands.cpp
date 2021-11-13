ae957066433c7df01e31b91f226e563281090a32
Martin Belko <martbelko@gmail.com>
2021-11-13 21:47:26 +0100

    Added some asserts

--- DominionEditor/src/Commands.cpp
+++ DominionEditor/src/Commands.cpp
@@ -7,15 +7,18 @@ namespace Dominion {
 	DuplicateEntityCommand::DuplicateEntityCommand(Entity srcEntity)
 		: mSrcEntity(srcEntity)
 	{
+		DM_ASSERT(mSrcEntity, "Invalid source entity");
 	}
 
 	void DuplicateEntityCommand::Do()
 	{
+		DM_ASSERT(mSrcEntity, "Invalid source entity");
 		mDstEntity = mSrcEntity.GetScene()->DuplicateEntity(mSrcEntity);
 	}
 
 	void DuplicateEntityCommand::Undo()
 	{
+		DM_ASSERT(mDstEntity, "Invalid destionation entity");
 		mDstEntity.GetScene()->DestroyEntity(mDstEntity);
 		mDstEntity = Entity();
 	}
@@ -23,15 +26,18 @@ namespace Dominion {
 	AddEntityCommand::AddEntityCommand(Ref<Scene>& scene, const std::string& name, UUID uuid)
 		: mScene(scene), mUUID(uuid), mName(name)
 	{
+		DM_ASSERT(mScene, "Scene was nullptr");
 	}
 
 	void AddEntityCommand::Do()
 	{
+		DM_ASSERT(mScene, "Scene was nullptr");
 		mEntity = mScene->CreateEntity(mUUID, mName);
 	}
 
 	void AddEntityCommand::Undo()
 	{
+		DM_ASSERT(mEntity, "Invalid entity");
 		mEntity.GetScene()->DestroyEntity(mEntity);
 	}
 
