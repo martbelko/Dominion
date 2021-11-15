9e5546ee1042fec38cfae12d8ce0c02a381d6f3e
Martin Belko <martbelko@gmail.com>
2021-11-15 15:46:51 +0100

    Updated for using Dominion::Texture2D instead of raw OpenGL

--- Dominion/src/Dominion/Renderer/FontRenderer.cpp
+++ Dominion/src/Dominion/Renderer/FontRenderer.cpp
@@ -55,26 +55,6 @@ namespace Dominion {
 			}
 
 			// generate texture
-			uint32_t texture;
-			glGenTextures(1, &texture);
-			glBindTexture(GL_TEXTURE_2D, texture);
-			glTexImage2D(
-				GL_TEXTURE_2D,
-				0,
-				GL_RED,
-				face->glyph->bitmap.width,
-				face->glyph->bitmap.rows,
-				0,
-				GL_RED,
-				GL_UNSIGNED_BYTE,
-				face->glyph->bitmap.buffer
-			);
-			// set texture options
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
 			Texture2DSpecification spec;
 			spec.width = face->glyph->bitmap.width;
 			spec.height = face->glyph->bitmap.rows;
