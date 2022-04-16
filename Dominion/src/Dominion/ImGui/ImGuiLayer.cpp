888b430deef638689eb7c0f34639c135757cc144
Martin Belko <martbelko@gmail.com>
2022-04-17 01:13:53 +0200

    Loading different consolas font type

--- Dominion/src/Dominion/ImGui/ImGuiLayer.cpp
+++ Dominion/src/Dominion/ImGui/ImGuiLayer.cpp
@@ -36,8 +36,8 @@ namespace Dominion {
 		//io.ConfigFlags |= ImGuiConfigFlags_ViewportsNoMerge;
 
 		float fontSize = 14.0f;// *2.0f;
-		io.Fonts->AddFontFromFileTTF("assets/fonts/consolas/Consolas.ttf", fontSize);
-		io.FontDefault = io.Fonts->AddFontFromFileTTF("assets/fonts/consolas/Consolas.ttf", fontSize);
+		io.Fonts->AddFontFromFileTTF("assets/fonts/consolas/CONSOLA.TTF", fontSize);
+		io.FontDefault = io.Fonts->AddFontFromFileTTF("assets/fonts/consolas/CONSOLA.TTF", fontSize);
 
 		// Setup Dear ImGui style
 		ImGui::StyleColorsDark();
