d30a2c988be7276a19a48ee2e162baee4c8fdf27
Martin Belko <martbelko@gmail.com>
2022-04-26 01:55:32 +0200

    Fixed settings panel rendering

--- DominionEditor/src/Panels/SettingsPanel.cpp
+++ DominionEditor/src/Panels/SettingsPanel.cpp
@@ -3,6 +3,7 @@
 #include <glm/gtc/type_ptr.hpp>
 
 #include <imgui/imgui.h>
+#include <imgui/imgui_internal.h>
 
 namespace Dominion {
 
@@ -10,9 +11,31 @@ namespace Dominion {
 	{
 		if (ImGui::Begin("Settings"))
 		{
-			ImGui::Checkbox("Show physics colliders", &mShowPhysicsColliders);
-			ImGui::DragFloat("Debug Line Thickness", &mDebugLineThickness, 0.01f, 0.001f, 10.0f);
-			ImGui::ColorEdit4("Debug Line Color", glm::value_ptr(m2DPhysicsCollidersColor));
+			constexpr float rightPadding = 10.0f;
+
+			{
+				constexpr const char* text = "Show physics colliders";
+				float width = ImGui::CalcTextSize(text).x + rightPadding;
+				ImGui::PushItemWidth(-width);
+				ImGui::Checkbox(text, &mShowPhysicsColliders);
+				ImGui::PopItemWidth();
+			}
+
+			{
+				constexpr const char* text = "Debug Line Thickness";
+				float width = ImGui::CalcTextSize(text).x + rightPadding;
+				ImGui::PushItemWidth(-width);
+				ImGui::DragFloat(text, &mDebugLineThickness, 0.01f, 0.001f, 10.0f);
+				ImGui::PopItemWidth();
+			}
+
+			{
+				constexpr const char* text = "Debug Line Color";
+				float width = ImGui::CalcTextSize(text).x + rightPadding;
+				ImGui::PushItemWidth(-width);
+				ImGui::ColorEdit4(text, glm::value_ptr(m2DPhysicsCollidersColor));
+				ImGui::PopItemWidth();
+			}
 		}
 		ImGui::End();
 	}
